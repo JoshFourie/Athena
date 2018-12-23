8674bffc4e2700b61e88154ac717b895188b8152
JoshFourie <josh@omniaprotocol.com>
2018-12-23 22:43:49 +1100

    orb: matches for Knowledgeable Orb

--- athena/src/lib.rs
+++ athena/src/lib.rs
@@ -4,4 +4,63 @@ extern crate itertools;
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
-pub mod zero_lib;
+pub mod zero_orb;
+
+#[test]
+fn test_athenian_knowledge() {
+    for _ in 0..100 {
+        let test = |input: [u8; 7], paths: [&Path; 4]| -> bool {
+            AthenianOrb::new(
+                vec![input[0]],
+                vec![input[1], input[2]],
+                b"Athenian".to_vec(),
+                paths
+            ).check(
+                vec![input[3], input[4]],
+                vec![input[5], input[6]],
+                paths
+            )
+        };
+        let paths = [
+            Path::new("src/zero_orb/source/athenian/athena_statement.zk"),
+            Path::new("src/zero_orb/source/athenian/athena_qap.json"),
+            Path::new("src/zero_orb/source/athenian/athena_sg1.json"),
+            Path::new("src/zero_orb/source/athenian/athena_sg2.json")
+        ]; 
+        assert_eq!(
+            true, 
+            test(
+                [10, 5, 15, 1, 0, 5, 15],
+                paths.clone()
+            )
+        );
+        assert_eq!(
+            false, 
+            test(
+                [20, 5, 15, 1, 0, 5, 15],
+                paths.clone()
+            )
+        );
+        assert_eq!(
+            false, 
+            test(
+                [0, 5, 15, 1, 0, 5, 15],
+                paths.clone()
+            )
+        );
+        assert_eq!(
+            false, 
+            test(
+                [10, 5, 20, 1, 0, 5, 15],
+                paths.clone()
+            )
+        );
+        assert_eq!(
+            true, 
+            test(
+                [12, 10, 15, 1, 0, 5, 15],
+                paths.clone()
+            )
+        );
+    }
+}
---
+++ athena/src/zero_orb/mod.rs
@@ -0,0 +1,434 @@
+use zksnark::*;
+use zksnark::field::z251::Z251;
+
+use std::path::Path;
+use std::fs::{File, read_to_string};
+use std::io::Write;
+use std::result::Result;
+
+use itertools::Itertools;
+use serde_json::{to_string, from_str};
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct AthenianOrb {
+    pub tag: Vec<u8>,
+    pub proof: Proof<Z251, Z251>,
+}
+
+pub struct CommonReference {
+    pub code: Vec<u8>,
+    pub qap: QAP<CoefficientPoly<Z251>>,
+    pub sg1: SigmaG1<Z251>,
+    pub sg2: SigmaG2<Z251>,
+}
+
+impl CommonReference {
+    pub fn new(paths: [&Path; 4]) {
+        let qap: QAP<CoefficientPoly<Z251>> = ASTParser::try_parse(
+            &read_to_string(paths[0]).unwrap()
+        ).unwrap().into();
+        let (sg1, sg2) = groth16::setup(&qap);
+        File::create(paths[1]).unwrap().write_all(
+            to_string(&qap).unwrap().as_bytes()
+        ).unwrap();
+        File::create(paths[2]).unwrap().write_all(
+            to_string(&sg1).unwrap().as_bytes()
+        ).unwrap();
+        File::create(paths[3]).unwrap().write_all(
+            to_string(&sg2).unwrap().as_bytes()
+        ).unwrap();
+    }
+    pub fn read(paths: [&Path; 4]) -> Self {
+        let code = read_to_string(paths[0]).unwrap().as_bytes().to_vec();
+        let qap : QAP<CoefficientPoly<Z251>> = from_str(
+            &read_to_string(paths[1]).unwrap()
+        ).unwrap();
+        let sg1 : SigmaG1<Z251> = from_str(
+            &read_to_string(paths[2]).unwrap()
+        ).unwrap();
+        let sg2 : SigmaG2<Z251> = from_str(
+            &read_to_string(paths[3]).unwrap()
+        ).unwrap();
+        Self {
+            code,
+            qap,
+            sg1,
+            sg2
+        }
+    }
+}
+
+pub trait IntoInnerField {
+    type Field;
+    fn collect_as_field(self) -> Result<Self::Field, ()>;
+} 
+
+impl IntoInnerField for Vec<u8> {
+    type Field = Vec<Z251>;
+    fn collect_as_field(self) -> Result<Self::Field, ()> {
+        let bit_array = self.into_iter().map(|mut num| {
+            let mut bits: [u8; 8] = [0; 8];
+            for i in 0..8 {
+                bits[i] = num % 2;
+                num = num >> 1;
+            }
+            bits
+        }).collect::<Vec<_>>();
+        Ok(
+            bit_array.into_iter()
+                .map(|a| {
+                a.iter().map(|&n| {
+                    assert!(n < 251);
+                    Z251 { inner: (n) as u8 }
+                }).collect::<Vec<_>>()        
+            }).concat()
+        )
+    }
+}
+
+impl IntoInnerField for Option<Vec<u8>> {
+    type Field = Vec<Z251>;
+    fn collect_as_field(self) -> Result<Self::Field, ()> {
+        match self {
+            Some(x) => {
+                let bit_array = x.into_iter()
+                    .map(|mut num| {
+                        let mut bits: [u8; 8] = [0; 8];
+                        for i in 0..8 {
+                            bits[i] = num % 2;
+                            num = num >> 1;
+                        }
+                        bits
+                    }).collect::<Vec<_>>();
+                return Ok(
+                    bit_array.into_iter()
+                        .map(|a| {
+                        a.iter().map(|&n| {
+                            assert!(n < 251);
+                            Z251 { inner: (n) as u8 }
+                        }).collect::<Vec<_>>()        
+                    }).concat()
+                )
+            },
+            None => Err(())
+        }
+    }
+}
+
+pub trait Knowledgeable {
+    fn new(
+        witness_num: Option<Vec<u8>>, variable_num: Option<Vec<u8>>,
+        witness_bits: Option<Vec<u8>>, variable_bits: Option<Vec<u8>>, 
+        tag: Vec<u8>, paths: [&Path; 4]) -> Self;
+    fn check(self, verify_num: Vec<u8>, verify_bits: Option<Vec<u8>>, paths: [&Path; 4]) -> bool;
+    fn as_bits(&self) -> Vec<u8>;
+}
+
+impl Knowledgeable for AthenianOrb {
+    fn new(
+        witness_num: Option<Vec<u8>>, 
+        variable_num: Option<Vec<u8>>,
+        witness_bits: Option<Vec<u8>>, 
+        variable_bits: Option<Vec<u8>>, 
+        tag: Vec<u8>, 
+        paths: [&Path; 4]
+    ) -> Self {
+        let crs = CommonReference::read(paths);
+        match (witness_num, variable_num, witness_bits, variable_bits
+            ){
+            (Some(witness_num), Some(variable_num), Some(witness_bits), Some(variable_bits)
+            ) => {
+                let mut assignments = witness_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                assignments.append(&mut variable_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>()
+                );
+                assignments.append(&mut witness_bits.collect_as_field().unwrap());
+                assignments.append(&mut variable_bits.collect_as_field().unwrap());
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (Some(witness_num), Some(variable_num), Some(witness_bits), None
+            ) => {
+                let mut assignments = witness_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                assignments.append(&mut variable_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>()
+                );
+                assignments.append(&mut witness_bits.collect_as_field().unwrap());
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (Some(witness_num), Some(variable_num), None, None
+            ) => {
+                let mut assignments = witness_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                assignments.append(&mut variable_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>()
+                );
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (Some(witness_num), None, None, None
+            ) => {
+                let assignments = witness_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (None, Some(variable_num), Some(witness_bits), Some(variable_bits)
+            ) => {
+                let mut assignments = variable_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                assignments.append(&mut witness_bits.collect_as_field().unwrap());
+                assignments.append(&mut variable_bits.collect_as_field().unwrap());
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (None, None, Some(witness_bits), Some(variable_bits)
+            ) => {
+                let mut assignments = witness_bits.collect_as_field().unwrap();
+                assignments.append(&mut variable_bits.collect_as_field().unwrap());
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (Some(witness_num), None, Some(witness_bits), None
+            ) => {
+                let mut assignments = witness_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                assignments.append(&mut witness_bits.collect_as_field().unwrap());
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (Some(witness_num), None, None, Some(variable_bits)
+            ) => {
+                  let mut assignments = witness_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                assignments.append(&mut variable_bits.collect_as_field().unwrap());
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (Some(witness_num), None, Some(witness_bits), Some(variable_bits)
+            ) => {
+                  let mut assignments = witness_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                assignments.append(&mut witness_bits.collect_as_field().unwrap());
+                assignments.append(&mut variable_bits.collect_as_field().unwrap());
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (Some(witness_num), Some(variable_num), None, Some(variable_bits)
+            ) => {
+                let mut assignments = witness_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                assignments.append(&mut variable_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>()
+                );
+                assignments.append(&mut variable_bits.collect_as_field().unwrap());
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (None, None, None, Some(variable_bits)
+            ) => {
+                let assignments = variable_bits.collect_as_field().unwrap();
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (None, None, Some(witness_bits), None
+            ) => {
+                let assignments = witness_bits.collect_as_field().unwrap();
+                let weights = groth16::weights(
+                    std::str::from_utf8(
+                        crs.code.as_slice()
+                    ).unwrap(), 
+                    &assignments
+                ).unwrap();    
+                Self {
+                    tag: tag,
+                    proof: groth16::prove(
+                        &crs.qap,
+                        (&crs.sg1, &crs.sg2),
+                        &weights
+                    ) 
+                }
+            },
+            (_, _, _, _) => panic!("Not yet implemnted."),         
+        }
+    }
+    fn check(self, verify_num: Vec<u8>, verify_bits: Option<Vec<u8>>, paths: [&Path; 4]) -> bool {
+        let crs = CommonReference::read(paths);
+        match verify_bits {
+            Some(_) => {
+                let mut inputs = verify_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                inputs.append(&mut verify_bits.collect_as_field().unwrap());
+                return groth16::verify::<CoefficientPoly<Z251>, _, _, _, _>(
+                    (crs.sg1, crs.sg2),
+                    &inputs,
+                    self.proof
+                )
+            },
+            None => {
+                let inputs = verify_num.into_iter()
+                    .map(|num: u8| Z251::from(num as usize))
+                    .collect::<Vec<_>>();
+                return groth16::verify::<CoefficientPoly<Z251>, _, _, _, _>(
+                    (crs.sg1, crs.sg2),
+                    &inputs,
+                    self.proof
+                )
+            },
+        };
+    }
+    fn as_bits(&self) -> Vec<u8> {
+        let string = to_string(&self).unwrap();
+        string.as_bytes().to_vec()
+    }
+}
